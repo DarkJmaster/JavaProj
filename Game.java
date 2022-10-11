bb4ec7c7251872e3b1594fc2d91b490d3146a9d0
DarkJMaster <Darkjknight@protonmail.com>
2022-10-11 07:37:09 -0700

    Random loot generation added &  package

---
+++ Game.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+//import java.util.Random;
+import loot_generation.loot;
+
+
+public class Game 
+{
+    public static void main(String args[])
+    {
+        loot a = new loot();
+        a.lowTier();
+    }
+}
